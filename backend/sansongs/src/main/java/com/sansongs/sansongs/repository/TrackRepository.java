d96da15680b514f15ff46bda7b0e1c91f80f9058
css <cauefc12@gmail.com>
2024-01-05 12:28:06 -0300

    TRACK CONTROLLER AND REPOSITORY ADDED

--- backend/sansongs/src/main/java/com/sansongs/sansongs/repository/TrackRepository.java
+++ backend/sansongs/src/main/java/com/sansongs/sansongs/repository/TrackRepository.java
@@ -1,5 +1,11 @@
 package com.sansongs.sansongs.repository;
 
-public class TrackRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sansongs.sansongs.model.Track;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Long> {
 
 }
